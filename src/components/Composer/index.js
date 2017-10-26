ae57c2a5104ab065d50f5effea3a01b7050a8284
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-10-26 14:14:15 +0300

    quick update

--- src/components/Composer/index.js
+++ src/components/Composer/index.js
@@ -9,6 +9,7 @@ export default class Composer extends Component {
     render () {
         return (
             <section className = { Styles.composer }>
+                <img src = { avatar } />
                 <form>
                     <textarea placeholder = { `What's on your mind, Homer?` } />
                     <input type = 'submit' value = 'Post' />
