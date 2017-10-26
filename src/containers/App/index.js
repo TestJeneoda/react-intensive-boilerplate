044f5d15211b25780161103a68253f008a19e2b6
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-10-26 21:31:02 +0300

    add Error Boundary

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -7,18 +7,26 @@ import avatar from '../../theme/assets/homer.png';
 
 // Components
 import Feed from '../../components/Feed';
+import Catcher from '../../components/Catcher';
+
+const GROUP_ID = 'l1lz1az2m5';
+const TOKEN = 'cc6j4usnne';
 
 const options = {
+    api:       `https://lab.lectrum.io/api/feed/${GROUP_ID}`,
     avatar,
     firstName: 'Homer',
-    lastName:  'Simpson'
+    lastName:  'Simpson',
+    token:     TOKEN
 };
 
 export default class App extends Component {
     static childContextTypes = {
+        api:       string.isRequired,
         avatar:    string.isRequired,
         firstName: string.isRequired,
-        lastName:  string.isRequired
+        lastName:  string.isRequired,
+        token:     string.isRequired
     };
 
     getChildContext () {
@@ -26,6 +34,10 @@ export default class App extends Component {
     }
 
     render () {
-        return <Feed />;
+        return (
+            <Catcher>
+                <Feed />
+            </Catcher>
+        );
     }
 }
