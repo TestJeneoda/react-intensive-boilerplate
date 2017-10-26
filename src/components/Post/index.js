cf614e3361207e81635cb4f9267b5a8684404eac
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-10-26 14:47:49 +0300

    quick update

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -10,6 +10,7 @@ export default class Post extends Component {
     render () {
         return (
             <section className = { Styles.post }>
+                <span className = { Styles.cross } />
                 <img src = { avatar } />
                 <a>Homer Simpson</a>
                 <time>{moment().format('MMMM D h:mm:ss a')}</time>
